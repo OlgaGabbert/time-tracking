73514e6679e148da4aef2f252863b24aaab2e457
Olga Gabbert <olga.gabbert@gmail.com>
2023-02-18 00:12:20 +0100

    update small cards

--- main.js
+++ main.js
@@ -1,26 +1,53 @@
-
-
-
-
 // // Fetch JSON Data
 // async function fetchData() {
 //     let response = await fetch("./data.json");
 //     let data = await response.json();
 
-//     // Save data into an array
-//     let arr = [];
-//     // for(let i in data) {
-//     //     arr.push([i, data [i]]);
-//     // }
-//     arr.push(data);
+// //     // Save data into an array
+// //     let arr = [];
+// //     // for(let i in data) {
+// //     //     arr.push([i, data [i]]);
+// //     // }
+// //     arr.push(data);
 
-//     // Iterate over array
-//     for(let i = 0; i<arr.length; i++) {
-//         console.log(arr[i]);
-//     }
+// //     // Iterate over array
+// //     for(let i = 0; i<arr.length; i++) {
+// //         console.log(arr[i]);
+// //     }
    
 
-// }
+//  }
 
 // // Call function 
 // fetchData();
+
+let daily = document.getElementById('daily');
+let weekly = document.getElementById('weekly');
+let monthly = document.getElementById('monthly');
+
+function changeToDaily() {
+    daily.style.color='white';
+    daily.style.fontWeight ='400';
+    weekly.style.color= '#DADADA';
+    weekly.style.fontWeight ='300';
+    monthly.style.color= '#DADADA';
+    monthly.style.fontWeight ='300';
+}
+
+function changeToWeekly() {
+    weekly.style.color='white';
+    weekly.style.fontWeight ='400';
+    daily.style.color= '#DADADA';
+    daily.style.fontWeight ='300';
+    monthly.style.color= '#DADADA';
+    monthly.style.fontWeight ='300';
+}
+
+function changeToMonthly() {
+    weekly.style.color= '#DADADA';
+    weekly.style.fontWeight ='300';
+    daily.style.color= '#DADADA';
+    daily.style.fontWeight ='300';
+    monthly.style.color='white';
+    monthly.style.fontWeight ='400';
+}
